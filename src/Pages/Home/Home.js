4472b0638e8ba6c4f64e144065fcc500b8ea92b5
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-06-08 20:35:18 +0600

    update home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,6 @@ import BusinessSection from './BusinessSection';
 import CompanyLogo from './CompanyLogo';
 import Contact from './Contact';
 import Review from './Review';
-
 import Tools from './Tools';
 
 const Home = () => {
